1421b9afb12171b14118e709b41ad59774d68ca0
정현규 <chunghyeongyu@jeonghyeongyuui-MacBookPro.local>
2025-01-15 18:27:32 +0900

    나머지 기능 완선

--- src/pages/showingpage/first.jsx
+++ src/pages/showingpage/first.jsx
@@ -1,14 +1,30 @@
-import { useNavigate } from "react-router-dom"
-import "./first.css"
+import { useNavigate } from "react-router-dom";
+import "./first.css";
 import Button from "../../atom/button";
-export default function First(){
-    const nav = useNavigate();
-    return(
-        <div className="first-container">
-            <div className="header">
-                <h2>BENDING MACHINE</h2>    
-            </div>
-            <Button onClick={()=>nav("/bending")} size="large" text="BENDINGMAC"></Button>
-        </div>
-    )
+import { useMoney } from "../../context/MoneyContext";
+export default function First() {
+  const nav = useNavigate();
+  const { money, setMoney } = useMoney();
+  const handleInputChange = (e) => {
+    if (isNaN(e.target.value)) {
+      alert("다시 입력해주세요.");
+    } else {
+      const value = parseInt(e.target.value);
+      setMoney(value);
+    }
+  };
+  return (
+    <div className="first-container">
+      <div className="header">
+        <h2>BENDING MACHINE</h2>
+        <span>소지금: </span>
+        <input type="text" value={money} onChange={handleInputChange} />
+      </div>
+      <Button
+        onClick={() => nav("/bending")}
+        size="large"
+        text="BENDINGMAC"
+      ></Button>
+    </div>
+  );
 }
